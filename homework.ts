0c911b38eb7de2300e8be913d9d01c5ab6d4925d
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-11-12 19:30:56 +0200

    restart

--- homework.ts
+++ homework.ts
@@ -1,65 +1,80 @@
 // 1
-interface IUser{
-    name:string
-    age:number,
-    gender:string
+interface IUser {
+    name: string
+    age: number,
+    gender: string
 }
 
-const user:IUser = {
-    name:'Max',
-    age:18,
-    gender:'male'
+const user: IUser = {
+    name: 'Max',
+    age: 18,
+    gender: 'male'
 }
 
-function  sum(a:number,b:number):number{
-   return (a+b);
+function sum(a: number, b: number): number {
+    return (a + b);
 }
 
-function showSum(a:number,b:number):void{
-    console.log(a+b);
+function showSum(a: number, b: number): void {
+    console.log(a + b);
 }
 
-function incAge(someUser:IUser,inc:number):IUser{
-    someUser.age+=inc;
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc;
     return someUser;
 }
 
 console.log(sum(1, 2));
-showSum(2,3)
+showSum(2, 3)
 incAge(user, 2)
 
 // 2
-interface IAnimal{
-    moving:string;
-    say:string;
-    Animal(info:string):string
+interface IAnimal {
+    moving: string;
+    say: string;
+
+    Animal(): string;
 }
 
-class Cat implements IAnimal{
-    moving: 'run';
-    say: 'give me a milk';
+class Cat implements IAnimal {
+    moving: string;
+    say: string;
 
-    Animal(info: string): string {
-        return 'Cat MoFo';
+    constructor() {
+        this.moving = 'run'
+        this.say = 'give me a milk';
+    }
+
+    Animal(): string {
+        return 'Cat ' + this.moving + ' and saying ' + this.say;
     }
 }
 
-class Bird implements IAnimal{
-    moving: 'fly';
-    say: 'tzwirin`-tzwirin`';
+class Bird implements IAnimal {
+    moving: string;
+    say: string;
 
-    Animal(info: string): string {
-        return 'я заплутався, це якась дічь';
+    constructor() {
+        this.moving = 'fly';
+        this.say = 'tzwirin`-tzwirin`';
+    }
+
+    Animal(): string {
+        return 'Bird ' + this.moving + ' and saying ' + this.say;
     }
 }
 
-class fish implements IAnimal{
-    moving: 'swim';
-    say: 'bul-bul';
+class fish implements IAnimal {
+    moving: string;
+    say: string;
 
-    Animal(info: string): string {
-        let x = 3+10
-        return info + 'я риба яка вміє рахувати';
+    constructor() {
+        this.moving = 'swim';
+        this.say = 'bul-bul';
+    }
+
+    Animal(): string {
+        return 'Fish ' + this.moving + ' and saying ' + this.say;
     }
 }
 
@@ -71,39 +86,38 @@ abstract class Shape {
     abstract perimeter(): number;
 }
 
-class Triangle extends Shape{
-    constructor(private a: number, private b: number){
+class Triangle extends Shape {
+    constructor(private a: number, private b: number) {
         super();
     }
 
     area(): number {
         console.log('Area', (<any>this).constructor.name)
-        return ((this.a*this.b)/2);
+        return ((this.a * this.b) / 2);
     }
 
     perimeter(): number {
         console.log('Perimeter', (<any>this).constructor.name)
-        return (((this.a^2+this.b^2)^(1/2))+this.a+this.b);
+        return (((this.a ^ 2 + this.b ^ 2) ^ (1 / 2)) + this.a + this.b);
     }
 }
 
-class Rectangle extends Shape{
+class Rectangle extends Shape {
     constructor(private a: number, private b: number) {
         super();
     }
+
     area(): number {
         console.log('Area', (<any>this).constructor.name)
-        return (this.a*this.b)
+        return (this.a * this.b)
     }
 
-    perimeter( ): number {
+    perimeter(): number {
         console.log('Perimeter', (<any>this).constructor.name)
-        return ((this.a+this.b)*2)
+        return ((this.a + this.b) * 2)
     }
 }
 
-const shapes: Shape[] = [new Triangle(3,4), new Rectangle(4,5)]
-// кладем в массив экземпляры классов(количество может быть любым но мин 2)
-// проходимся циклом по нему и и высчитываем площадь для каждой фигуры
+const shapes: Shape[] = [new Triangle(3, 4), new Rectangle(4, 5)]
 
 
